34e954e42af947d3116b7d10bd63217bb5d7a7c2
rmd3 <michaeldanjolell@gmail.com>
2022-03-13 17:52:08 -0400

    Add project files.

---
+++ D'Anjolell_8_9/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace D_Anjolell_8_9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnSubmit.Location = new System.Drawing.Point(248, 246);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(127, 52);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnExit.Location = new System.Drawing.Point(417, 246);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(127, 52);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.txtPhone.Location = new System.Drawing.Point(521, 132);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(250, 38);
+            this.txtPhone.TabIndex = 2;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.lblTitle.Location = new System.Drawing.Point(74, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(640, 39);
+            this.lblTitle.TabIndex = 3;
+            this.lblTitle.Text = "Alphabetic Telephone Number Translator";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblPhone.Location = new System.Drawing.Point(12, 135);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(467, 31);
+            this.lblPhone.TabIndex = 4;
+            this.lblPhone.Text = "Enter phone number with hyphens(-): ";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblResult.Location = new System.Drawing.Point(242, 336);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 31);
+            this.lblResult.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSubmit);
+            this.Name = "Form1";
+            this.Text = "Alphabetic Telephone Number Translator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ D'Anjolell_8_9/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace D_Anjolell_8_9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Closes the window whem the user clicks the button "Exit"
+            this.Close();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //Richard D'Anjolell
+            //Declare Variables
+            string userPhone = txtPhone.Text;
+            string finalPhoneNumber;
+            const int ARRAY_SIZE = 12;
+            char[] phoneNumber = new char[ARRAY_SIZE];
+            
+            //If statements to make sure entered string is valid
+            if (userPhone.Length != ARRAY_SIZE) 
+            {
+                lblResult.Text = "Length of phone number is incorrect.";
+            }
+            else if (!userPhone.Contains('-'))
+            {
+                lblResult.Text = "Hyphen aren't in the phone number.";
+            }
+            else if (userPhone.Any(char.IsLower))
+            {
+                lblResult.Text = "Lowercase letters not allowed.";
+            }
+            else 
+            {
+                //Goes through the entered phone number and turns any alphabetic characters into a number
+                for (int i = 0; i < ARRAY_SIZE; i++)
+                {
+                    if (char.IsDigit(userPhone[i]))
+                    {
+                        phoneNumber[i] = userPhone[i];
+                    }
+                    else if (userPhone[i] == 'A' || userPhone[i] == 'B' || userPhone[i] == 'C')
+                    {
+                        phoneNumber[i] = '2';
+                    }
+                    else if (userPhone[i] == 'D' || userPhone[i] == 'E' || userPhone[i] == 'F')
+                    {
+                        phoneNumber[i] = '3';
+                    }
+                    else if (userPhone[i] == 'G' || userPhone[i] == 'H' || userPhone[i] == 'I')
+                    {
+                        phoneNumber[i] = '4';
+                    }
+                    else if (userPhone[i] == 'J' || userPhone[i] == 'K' || userPhone[i] == 'L')
+                    {
+                        phoneNumber[i] = '5';
+                    }
+                    else if (userPhone[i] == 'M' || userPhone[i] == 'N' || userPhone[i] == 'O')
+                    {
+                        phoneNumber[i] = '6';
+                    }
+                    else if (userPhone[i] == 'P' || userPhone[i] == 'Q' || userPhone[i] == 'R' || userPhone[i] == 'S')
+                    {
+                        phoneNumber[i] = '7';
+                    }
+                    else if (userPhone[i] == 'T' || userPhone[i] == 'U' || userPhone[i] == 'V')
+                    {
+                        phoneNumber[i] = '8';
+                    }
+                    else if (userPhone[i] == 'W' || userPhone[i] == 'X' || userPhone[i] == 'Y' || userPhone[i] == 'Z')
+                    {
+                        phoneNumber[i] = '9';
+                    }
+                    //Else for the hyphens
+                    else
+                    {
+                        phoneNumber[i] = userPhone[i];
+                    }
+                }
+                //Turns the array of numbers into a string
+                finalPhoneNumber = new string(phoneNumber);
+                //Outputs said string
+                lblResult.Text = finalPhoneNumber;
+            }
+        }
+    }
+}
